bbdaccdeda1891c22146e96c7d2ab37ba55a4ae2
Jarryd Martin <jarryd@codora.co.nz>
2021-03-09 14:51:57 +1300

    fix

--- components/ArticleIndexAside.tsx
+++ components/ArticleIndexAside.tsx
@@ -1,25 +1,29 @@
 import { List, ListItem, ListItemIcon, ListItemText } from "@material-ui/core";
 import Link from "next/link";
+import { useRouter } from "next/router";
 import React, { useEffect, useState } from "react";
 import { GetArticleTypes } from "../lib/dataAccess";
 
 const ArticleIndexAside = () => {
+  const router = useRouter();
+  const slug = router.query['slug'] as string
   const [articleTypes, setArticleTypes] = useState(null);
 
   const getData = async () => {
     const data = await GetArticleTypes();
+  
     setArticleTypes(
       data
         .sort((a, b) => (a.order > b.order ? 1 : -1))
         .map((a) => (
-          <ArticleTypeItem key={a.slug} to={`/article/${a.slug}`} primary={a.name} />
+          <ArticleTypeItem key={a.slug} to={`/article/${a.slug}`} primary={a.name}  isSelected={a.slug === slug} />
         ))
     );
   };
 
   useEffect(() => {
     getData();
-  }, []);
+  }, [router]);
   return (
     <div>
       <h2>Index</h2>
@@ -31,10 +35,10 @@ const ArticleIndexAside = () => {
 };
 
 function ArticleTypeItem(props) {
-  const { icon, primary, to } = props;
+  const { icon, primary, to, isSelected } = props;
   return (
     <Link href={to}>
-      <ListItem button>
+      <ListItem button selected={isSelected}>
         <ListItemText primary={primary} />
       </ListItem>
     </Link>
