25af9a240cba7c1d5b3f0baf41e995cc89a06f7b
Jarryd Martin <jarryd@codora.co.nz>
2021-03-15 10:24:04 +1300

    Ui change and refactored likes

--- lib/adminScripts.ts
+++ lib/adminScripts.ts
@@ -1,14 +1,25 @@
 // scripts to help with admin tasks while developing
 
-import { GetArticles, UpdateArticle } from "./dataAccess";
+import { AddLike, GetArticles, UpdateArticle } from "./dataAccess";
 
 // Call to run updates on all artcoles
 export const ADMIN_updateArticles = async () => {
-    console.log("Running ADMIN_updateArticles...");
-    const artciles = await GetArticles();
+    // console.log("Running ADMIN_updateArticles...");
+    // const artciles = await GetArticles();
 
-    artciles.forEach((article) => {
+    // artciles.forEach((article) => {
+    //     console.log(`Updating ${article.articleId}....`);
+    //     UpdateArticle({ ...article, likes: [] });
+    // });
+
+    const artciles = await GetArticles();
+    console.log("Running ADMIN_updateArticles...");
+    artciles.forEach(article => {
         console.log(`Updating ${article.articleId}....`);
-        UpdateArticle({ ...article, likes: [] });
+        article.likes.forEach(like => {
+            console.log(`like: ${like}....`);
+            AddLike(article.articleId, like);
+        })
     });
 };
+
